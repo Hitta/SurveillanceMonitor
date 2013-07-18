15fa225fcdf0eafb3fbe1e6f9bf0ef0d6d29dfb8
Mats Bovin <mats.bovin@gmail.com>
2013-07-18 09:32:42 +0200

    Added HttpBodyCompareDataCollector

---
+++ Hitta.Surveillance.Monitor/DataCollectors/HttpBodyCompareDataCollector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hitta.Surveillance.Monitor.DataCollectors
+{
+    public class HttpBodyCompareDataCollector : HttpDataCollector
+    {
+        private string url2;
+
+        public HttpBodyCompareDataCollector(string displayName, string description, int interval, string url, string url2)
+            : base(displayName, description, interval, url)
+        {
+            this.url2 = url2;
+        }
+
+        protected override int GetResponseInternal(string response)
+        {
+            try
+            {
+                string response2 = client.DownloadString(url2);
+                return response.CompareTo(response2);
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+    }
+}
